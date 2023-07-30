68d3713ae90967abc0de56eb2fc6242b7fb8a1bf
rambhardwajj <rambhardwaj101000@gmail.com>
2023-07-30 10:51:20 +0530

    SignIn page complete

--- FrontEnd/src/SignIn.jsx
+++ FrontEnd/src/SignIn.jsx
@@ -3,27 +3,31 @@ import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import { useState } from 'react';
 import { Typography } from '@mui/material';
+import moveImgg from './moveee.jpeg'
 
 function SignIn(){
-
 	const [email , setEmail] = useState("");
 	const [password, setPassword] =  useState("");
-	return <div>
-		<div style={{ display : "flex", justifyContent : 'center'}}>
-			<div style={{
-				paddingTop  : 150
-				}}> 
-				<Typography  variant={"h6"}>
-					Welcome Back to course App , Sign In below 
+	return <div style={{ backgroundImage: `url(${moveImgg})`, backgroundSize: 'cover',    minHeight: '100vh' }}>
+		<div style={{  display : "flex", justifyContent : 'center'}}>
+			<div style={{ paddingTop  : 60  }}> 
+				<Typography  borderRadius={2} padding={2} bgcolor={'black'} fontFamily={'Impact, sans-serif'} color={ '#eeeeee' }  variant={"h4"}>
+					Welcome Back to course App ,  
+					<br />
+					Sign In below 
 				</Typography>  
+				<br /> <br />
 			</div>
 		</div>
 		<div style={{ display : "flex", justifyContent : 'center'}}>
 			<Card variant="outlined"  style={{ 
+					borderRadius: 10,
+					backgroundColor: 'black',
 					width : 400,
 					padding : 20
 			}}>
       			<TextField 
+					style={{  borderRadius:10,  backgroundColor: '#eeeeee'}}
 					onChange={(e) =>{
 						setEmail(e.target.value);
 					}}
@@ -32,6 +36,7 @@ function SignIn(){
 					variant="outlined"/>
 				<br /> <br />
 				<TextField 
+					style={{  borderRadius:10,  backgroundColor: '#eeeeee'}}
 					onChange={(e) =>{
 						setPassword( e.target.value);
 					}}
@@ -45,6 +50,7 @@ function SignIn(){
 					onClick={()=>{
 						function callback2(data){
 							localStorage.setItem("token", data.token);
+							window.location= '/courses'
 							console.log(data);
 						}
 						function callback1(res){
